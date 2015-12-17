3be17e60f0d294940a6c96280cd189c88a9093d5
threezj <27602942@qq.com>
2015-12-17 16:41:43 +0800

    try custom view

---
+++ CustionView/app/src/main/java/com/example/three/custionview/MyAdapter.java
@@ -0,0 +1,33 @@
+package com.example.three.custionview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Zj on 2015/12/17.
+ */
+public class MyAdapter extends ArrayAdapter<String> {
+    public MyAdapter(Context context, int textViewResourceId, List<String> objects) {
+        super(context, textViewResourceId, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.my_list_view_item, null);
+        } else {
+            view = convertView;
+        }
+        TextView textView = (TextView) view.findViewById(R.id.text_view);
+        textView.setText(getItem(position));
+        return view;
+    }
+
+}
